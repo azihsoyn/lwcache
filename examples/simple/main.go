2dd0f187f9e329a71ee39e80513055addbde35a3
azihsoyn <azihsoyn@gmail.com>
2016-08-26 15:08:04 +0900

    align sample output

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	c := cache.New("sample")
-	fmt.Println("now : ", time.Now())
+	fmt.Println("now : ", time.Now().Format("2006-01-02 15:04:05"))
 	c.Set(1, "Apple", 1*time.Second)
 	c.Set(2, "Banana", 2*time.Second)
 	c.Set(3, "Cake", 3*time.Second)
@@ -17,7 +17,7 @@ func main() {
 	for j := 0; j < 4; j++ {
 		for i := 1; i <= 3; i++ {
 			v, ok := c.Get(i)
-			fmt.Printf("value : %v, ok : %t, now : %v\n", v, ok, time.Now())
+			fmt.Printf("value : %6v, ok : %5t, now : %v\n", v, ok, time.Now().Format("2006-01-02 15:04:05"))
 		}
 		time.Sleep(1 * time.Second)
 	}
