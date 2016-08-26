5f88242088499783dcd2de7ff7f3ebce60a23f61
azihsoyn <azihsoyn@gmail.com>
2016-08-26 17:58:30 +0900

    add test

--- cache_test.go
+++ cache_test.go
@@ -50,9 +50,9 @@ func TestSetExpire(t *testing.T) {
 
 func TestSetRefresher(t *testing.T) {
 	assert := assert.New(t)
-	c := New("test-3")
+	c := New("test-4")
 
-	key := "test-3"
+	key := "test-4"
 	expect := 0
 	refresher := func(key interface{}, currentValue interface{}) (interface{}, error) {
 		num, ok := currentValue.(int)
@@ -71,5 +71,60 @@ func TestSetRefresher(t *testing.T) {
 		assert.Equal(expect, actual, fmt.Sprintf("Test No. %d", i+1))
 		time.Sleep(1050 * time.Millisecond)
 		expect++
+	}
+}
+
+func TestSetRefresher_OnExpired(t *testing.T) {
+	assert := assert.New(t)
+	c := New("test-5")
+
+	key := "test-5"
+	expect := 0
+	refresher := func(key interface{}, currentValue interface{}) (interface{}, error) {
+		num, ok := currentValue.(int)
+		if ok {
+			return num + 1, nil
+		}
+		return 0, errors.New("refresh failed")
+	}
+
+	c.Set(key, expect, 2*time.Second)
+	c.SetRefresher(key, refresher, 1*time.Second)
+
+	actual, ok := c.Get(key)
+	assert.True(ok)
+	assert.Equal(expect, actual)
+	time.Sleep(1050 * time.Millisecond)
+
+	// refresh
+	actual, ok = c.Get(key)
+	assert.True(ok)
+	assert.Equal(expect+1, actual)
+	time.Sleep(1050 * time.Millisecond)
+
+	// expire
+	actual, ok = c.Get(key)
+	assert.False(ok)
+	assert.Equal(nil, actual)
+}
+
+func TestSetRefresher_OnRefreshError(t *testing.T) {
+	assert := assert.New(t)
+	c := New("test-3")
+
+	key := "test-3"
+	expect := 0
+	refresher := func(key interface{}, currentValue interface{}) (interface{}, error) {
+		return 0, errors.New("refresh failed")
+	}
+
+	c.Set(key, expect, 10*time.Second)
+	c.SetRefresher(key, refresher, 1*time.Second)
+
+	for i := 0; i < 5; i++ {
+		actual, ok := c.Get(key)
+		assert.True(ok)
+		assert.Equal(expect, actual, fmt.Sprintf("Test No. %d", i+1))
+		time.Sleep(1050 * time.Millisecond)
 	}
 }
